3428bb4bb02f66d85d7b29a0036bef71b3a698a8
NATHANiothy03 <iothynathan30@gmail.com>
2026-06-08 16:26:02 +0300

    Projet

---
+++ Contact.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SC</title>
+    <link rel="stylesheet" href="bootstrap-5.3.8-dist/css/bootstrap.css">
+</head>
+<body class="bg-dark  text-light">
+     <nav class="navbar navbar-expand-lg bg-body-tertiary"  data-bs-theme="dark">
+  <div class="container-fluid">
+    <img src="ST.PNG"  class="rounded-circle" alt="Logo" width="60" height="50" class="d-inline-block align-text-top">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="Page_d'accueil.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Produits.php">Produits</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Panier.php">Panier</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
---
+++ Page_d'accueil.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SC</title>
+    <link rel="stylesheet" href="bootstrap-5.3.8-dist/css/bootstrap.css">
+</head>
+<body class="bg-dark text-light">
+<nav class="navbar navbar-expand-lg bg-body-tertiary"  data-bs-theme="dark">
+  <div class="container-fluid">
+    <img src="ST.PNG"  class="rounded-circle" alt="Logo" width="60" height="50" class="d-inline-block align-text-top">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Produits.php">Produits</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Contact.php">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Panier.php">Panier</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
+<h1 class="text-center">School Things(ST)</h1>
+<p>ST est une entreprise fournissant les fournitures scolaires. Elle a pour PDG "Mr NATHAN",age de 17ans, il a cree l'entreprise afin d'aider les ecoles publics afin qu ils puissent travailler avec les meilleurs ustensils d'etudes.</p>
+<p>L'entrepise a ete fonde en 2013 , et a realisee un chiffre de 1.000.000$ en 2018. Ainsi le niveau d'etude a Madagascar pendants les 13 annees fut ameliore.</p>
+</body>
+</html>
